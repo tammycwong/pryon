57413125b6060374f1910175f4fda16199bc4532
Tammy <tammycwong@gmail.com>
2021-11-02 23:23:56 -0400

    removed sass

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Auth0Provider } from "@auth0/auth0-react";
-import "./scss/main.scss";
+import "./App.css";
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
 
