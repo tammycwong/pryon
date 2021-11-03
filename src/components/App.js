3a596668f10260ffb5b53e3bdc297370651d5c58
Tammy <tammycwong@gmail.com>
2021-11-03 08:10:45 -0400

    removed border

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,33 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import Menu from "./Menu";
+import Profile from "./Profile";
+import Home from "./Home";
+import Astronauts from "./Astronauts";
+import IssLocation from "./IssLocation";
 
 export default function App() {
-
   return (
-    <div>
+    <>
       <Menu />
-    </div>
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+
+        <Route exact path="/profile">
+          <Profile />
+        </Route>
+
+        <Route exact path="/astronauts">
+          <Astronauts />
+        </Route>
+
+        <Route exact path="/isslocation">
+          <IssLocation />
+        </Route>
+      </Switch>
+    </>
   );
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,9 +1,5 @@
 import React from "react";
-import { Link, Switch, Route } from "react-router-dom";
-import Profile from "./Profile";
-import Home from "./Home";
-import Astronauts from "./Astronauts";
-import IssLocation from "./IssLocation";
+import { Link } from "react-router-dom";
 import logo from "../images/main-logo.png";
 import { useAuth0 } from "@auth0/auth0-react";
 
@@ -13,65 +9,49 @@ export default function Menu() {
   return (
     <div className="menu">
       <div className="links">
-
         <Link className="menu-link" to="/">
           <img className="logo" src={logo} alt="logo" />
         </Link>
 
-          {!isAuthenticated && (
-            <Link className="menu-link" onClick={() => loginWithRedirect()}>
-              Log In
-            </Link>
-          )}
+        {!isAuthenticated && (
+          <Link className="menu-link" onClick={() => loginWithRedirect()}>
+            Log In
+          </Link>
+        )}
 
-          {!isAuthenticated && (
-            <Link className="menu-link" onClick={() => loginWithRedirect({screen_hint:'signup'})}>
-              Sign Up
-            </Link>
-          )}
+        {!isAuthenticated && (
+          <Link
+            className="menu-link"
+            onClick={() => loginWithRedirect({ screen_hint: "signup" })}
+          >
+            Sign Up
+          </Link>
+        )}
 
-          {isAuthenticated ? (
-            <Link className="menu-link" to="/profile">
-              Profile
-            </Link>
-          ) : null}
+        {isAuthenticated ? (
+          <Link className="menu-link" to="/profile">
+            Profile
+          </Link>
+        ) : null}
 
-          {isAuthenticated ? (
-            <Link className="menu-link" to="/astronauts">
-              Astronauts
-            </Link>
-          ) : null}
+        {isAuthenticated ? (
+          <Link className="menu-link" to="/astronauts">
+            Astronauts
+          </Link>
+        ) : null}
 
-          {isAuthenticated ? (
-            <Link className="menu-link" to="/isslocation">
-              ISS Location
-            </Link>
-          ) : null}
+        {isAuthenticated ? (
+          <Link className="menu-link" to="/isslocation">
+            ISS Location
+          </Link>
+        ) : null}
 
-          {isAuthenticated && (
-            <Link onClick={() => logout()} className="menu-link">
-              Log Out
-            </Link>
-          )}
+        {isAuthenticated && (
+          <Link onClick={() => logout()} className="menu-link">
+            Log Out
+          </Link>
+        )}
       </div>
-
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-
-        <Route exact path="/profile">
-          <Profile />
-        </Route>
-
-        <Route exact path="/astronauts">
-          <Astronauts />
-        </Route>
-
-        <Route exact path="/isslocation">
-          <IssLocation />
-        </Route>
-      </Switch>
     </div>
   );
 }
